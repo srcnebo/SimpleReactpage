d344dceeed0891332df613ca1c0358db9bdfea5e
Steven <stv.nebo@gmail.com>
2018-10-19 12:01:13 +0300

    Updates

---
+++ precache-manifest.67033e661ca005c2990c4bdc7e7498df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9e24e1b95fa43a3f93aa3aa8315a988",
+    "url": "/SimpleReactpage/static/media/img-placeholder.a9e24e1b.png"
+  },
+  {
+    "revision": "99f7456dfd298c4545c7687ce8d5539d",
+    "url": "/SimpleReactpage/static/media/Sansation_Light.99f7456d.ttf"
+  },
+  {
+    "revision": "ed9223b46a7767df0f842300f83ce157",
+    "url": "/SimpleReactpage/static/media/Sansation_Bold_Italic.ed9223b4.ttf"
+  },
+  {
+    "revision": "e15941b94fb34ad1d6854b540cb95a92",
+    "url": "/SimpleReactpage/static/media/Header-img.e15941b9.png"
+  },
+  {
+    "revision": "1e8bb7fb3ea65123dcbb56b4069b15fb",
+    "url": "/SimpleReactpage/static/media/Border-BG.1e8bb7fb.jpg"
+  },
+  {
+    "revision": "89cd82ee3d19f3b0f6f4",
+    "url": "/SimpleReactpage/static/js/runtime~main.89cd82ee.js"
+  },
+  {
+    "revision": "b2b3409fbb9039a78485",
+    "url": "/SimpleReactpage/static/js/main.b2b3409f.chunk.js"
+  },
+  {
+    "revision": "904a9ce4ed9a253d1cfa",
+    "url": "/SimpleReactpage/static/js/1.904a9ce4.chunk.js"
+  },
+  {
+    "revision": "b2b3409fbb9039a78485",
+    "url": "/SimpleReactpage/static/css/main.667e4220.chunk.css"
+  },
+  {
+    "revision": "111eb280152f16c98e94dcd5a8172200",
+    "url": "/SimpleReactpage/index.html"
+  }
+];
